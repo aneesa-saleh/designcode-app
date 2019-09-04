9945cf0291437d55a9fbd23491e02cdf12b7f67b
Aneesa Saleh <aneesa.saleh@gmail.com>
2019-09-04 21:15:30 +0100

    Add profile image and subtitle to home page

--- App.js
+++ App.js
@@ -5,14 +5,55 @@ import styled from 'styled-components';
 export default function App() {
   return (
     <Container>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TitleBar>
+        <Avatar source={require('./assets/avatar.jpg')} />
+        <Title>Welcome back</Title>
+        <Name>Mengi</Name>
+      </TitleBar>
+      <Subtitle>Continue Learning</Subtitle>
     </Container>
   );
 }
 
+const Subtitle = styled.Text`
+  color: #b8bece;
+  font-weight: 600;
+  font-size: 15px;
+  margin-left: 20px;
+  margin-top: 50px;
+  text-transform: uppercase;
+`;
+
+const Avatar = styled.Image`
+  width: 44px;
+  height: 44px;
+  background: black;
+  border-radius: 22px;
+  margin-left: 22px;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
 const Container = styled.View`
   flex: 1;
-  background-color: pink;
-  align-items: center;
-  justify-content: center;
+  background-color: #f0f3f5;
+`;
+
+const Title = styled.Text`
+  font-size: 16px;
+  color: #b8bece;
+  font-weight: 500;
+`;
+
+const Name = styled.Text`
+  font-size: 20px;
+  color: #3c4560;
+  font-weight: bold;
+`;
+
+const TitleBar = styled.View`
+  width: 100%;
+  margin-top: 50px;
+  padding-left: 80px;
 `;
