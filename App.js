1b6dc349b7546f6175ced750561ffe25f7aced47
Aneesa Saleh <aneesa.saleh@gmail.com>
2019-09-08 22:21:48 +0100

    Create card component

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Text } from 'react-native';
 import styled from 'styled-components';
+import Card from './components/card';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Name>Mengi</Name>
       </TitleBar>
       <Subtitle>Continue Learning</Subtitle>
+      <Card />
     </Container>
   );
 }
