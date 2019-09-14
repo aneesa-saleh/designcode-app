b3916ad1440ad261d845d716a8a14cf535950250
Aneesa Saleh <aneesa.saleh@gmail.com>
2019-09-14 18:45:43 +0100

    Add static data for cards

--- App.js
+++ App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { ScrollView, SafeAreaView } from 'react-native';
 import styled from 'styled-components';
 import Card from './components/card';
-import { Ionicons } from '@expo/vector-icons';
 import { NotificationIcon } from './components/Icons';
 
 import cardBackround from './assets/background2.jpg';
 import reactLogo from './assets/logo-react.png';
 import avatar from './assets/avatar.jpg';
+import framerxLogo from './assets/logo-framerx.png';
+import figmaLogo from './assets/logo-figma.png';
+import studioLogo from './assets/logo-studio.png';
+import swiftLogo from './assets/logo-swift.png';
+import sketchLogo from './assets/logo-sketch.png';
+
+import Logo from './components/Logo';
 
 export default function App() {
   return (
@@ -20,6 +26,11 @@ export default function App() {
             <Name>Mengi</Name>
             <StyledNotificationIcon />
           </TitleBar>
+          <LogoWrapper horizontal showsHorizontalScrollIndicator={false}>
+            {logos.map((logo, index) => (
+              <Logo key={index} image={logo.image} text={logo.text} />
+            ))}
+          </LogoWrapper>
           <Subtitle>Continue Learning</Subtitle>
           <CardWrapper horizontal showsHorizontalScrollIndicator={false}>
             <Card
@@ -43,6 +54,13 @@ export default function App() {
   );
 }
 
+const LogoWrapper = styled.ScrollView`
+  flex-direction: row;
+  padding: 20px;
+  padding-left: 10px;
+  padding-top: 30px;
+`;
+
 const StyledNotificationIcon = styled(NotificationIcon)`
   position: absolute;
   right: 20px;
@@ -58,7 +76,7 @@ const Subtitle = styled.Text`
   font-weight: 600;
   font-size: 15px;
   margin-left: 20px;
-  margin-top: 50px;
+  margin-top: 20px;
   text-transform: uppercase;
 `;
 
@@ -95,3 +113,30 @@ const TitleBar = styled.View`
   margin-top: 50px;
   padding-left: 80px;
 `;
+
+const logos = [
+  {
+    image: framerxLogo,
+    text: 'Framer X'
+  },
+  {
+    image: figmaLogo,
+    text: 'Figma'
+  },
+  {
+    image: studioLogo,
+    text: 'Studio'
+  },
+  {
+    image: reactLogo,
+    text: 'React'
+  },
+  {
+    image: swiftLogo,
+    text: 'Swift'
+  },
+  {
+    image: sketchLogo,
+    text: 'Sketch'
+  }
+];
